c9e09df99295db8b20f4bef98776db4027677d9e
pooja-kh-26 <poojakh2026@gmail.com>
2024-09-10 22:07:50 +0530

    first javascript project

--- app.js
+++ app.js
@@ -2,3 +2,43 @@ let boxes = document.querySelectorAll(".box");
 let resetBtn = document.querySelector("#reset-btn");
 
 let turnX = true;
+
+const winPatterns = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+];
+
+boxes.forEach( (box) => {
+    box.addEventListener("click", () => {
+        if(turnX){
+            box.innerText = "X";
+            turnX = false;
+        }else{
+            box.innerText = "O";
+            turnX = true;
+        }
+        box.disabled = true;
+
+        checkWinner();
+    });
+});
+
+const checkWinner = () =>{
+    for(let pattern of winPatterns){
+        let pos1value = boxes[pattern[0]].innerText;
+        let pos2value = boxes[pattern[1]].innerText;
+        let pos3value = boxes[pattern[2]].innerText;
+
+        if(pos1value !="" && pos2value !="" && pos3value !=""){
+            if(pos1value === pos2value && pos2value === pos3value){
+                
+            }
+        }
+    }
+};
